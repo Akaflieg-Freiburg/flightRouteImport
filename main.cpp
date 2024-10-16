a7b10031fc2f7b16d68788639b963ad8b1a93b39
bloech1 <130927974+bloech1@users.noreply.github.com>
2024-10-16 17:41:55 +0200

    Support FPL format

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <QCommandLineParser>
 
 #include "PLN.h"
+#include "FPL.h"
 
 auto main(int argc, char *argv[]) -> int
 {
@@ -16,14 +17,24 @@ auto main(int argc, char *argv[]) -> int
     {
         qCritical() << "File name required.";
         return -1;
-
     }
 
-    qWarning() << "Reading file as PLN" << parser.positionalArguments().at(0);
-    FileFormats::PLN PLN(parser.positionalArguments().at(0));
-    qWarning() << "Errors while reading:" << PLN.error();
-    qWarning() << "Warnings while reading:" << PLN.warnings();
-    qWarning() << "Waypoint list:" << PLN.waypoints();
+    if (parser.positionalArguments().at(0).endsWith("PLN", Qt::CaseInsensitive))
+    {
+        qWarning() << "Reading file as PLN" << parser.positionalArguments().at(0);
+        FileFormats::PLN PLN(parser.positionalArguments().at(0));
+        qWarning() << "Errors while reading:" << PLN.error();
+        qWarning() << "Warnings while reading:" << PLN.warnings();
+        qWarning() << "Waypoint list:" << PLN.waypoints();
+    }
+    else
+    {
+        qWarning() << "Reading file as FPL" << parser.positionalArguments().at(0);
+        FileFormats::FPL FPL(parser.positionalArguments().at(0));
+        qWarning() << "Errors while reading:" << FPL.error();
+        qWarning() << "Warnings while reading:" << FPL.warnings();
+        qWarning() << "Waypoint list:" << FPL.waypoints();
+    }
 
     return 0;
 }
